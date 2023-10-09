9df370c17b95e28f69c009a5303c5a609877f720
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-10-09 21:52:36 +1300

    Add initial version of PMC

---
+++ Assets/Scripts/PlayerMotionController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("Controls")]
+    [SerializeField] public KeyCode jumpCode = KeyCode.Space;
+
+    [Header("Movement")]
+    [SerializeField] public float movementSpeed;
+    [SerializeField] public float attackingMovementSpeedMultiplier = 0.9f;
+
+    [Header("Attacking")]
+    [SerializeField] public bool southpaw = false;
+    [SerializeField] public float attackCooldown;
+    /*
+     * When an attack is made, elapsed cooldown is set to attack cooldown.
+     * As time passed, elapsed cooldown is decreased until it becomes 0.
+     * The player can only attack when elapsedCooldown is 0.
+     */
+    private float elapsedCooldown;
+
+    // Animation Variables
+    private bool _isGrounded;
+
+    // Player transform.
+    Transform playerTransform;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+       this.playerTransform = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+        bool attack = Input.GetMouseButton(southpaw ? 2 : 1);
+        
+    }
+
+    private void Move(float vAxis, float hAxis)
+    {
+        Vector3 motionOffset = (playerTransform.forward * vAxis) + (playerTransform.right * hAxis);
+    }
+}
