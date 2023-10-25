b65e6d536d0132bc779eb2d3e022b46cfeb41e04
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-10-25 19:40:08 +1300

    Create manager to set effect settings

---
+++ Assets/Scripts/UI/PostProcessingManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingManager : MonoBehaviour
+{
+    public static PostProcessingManager Instance { get; private set; }
+
+    [SerializeField] private PostProcessVolume _lowHealthEffect;
+    [SerializeField] private PostProcessVolume _lightingAndBloomEffect;
+
+    Vignette vignette;
+
+    private void OnEnable()
+    {
+        _lowHealthEffect.profile.TryGetSettings(out vignette);
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            // DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void setVignetteIntensity(float healthPercentage)
+    {
+        float missingHealthPercentage = 1 - healthPercentage;
+        Debug.Log(missingHealthPercentage);
+        FloatParameter newIntensity = new FloatParameter { value = missingHealthPercentage };
+        vignette.intensity.value = missingHealthPercentage;
+        Debug.Log("Vignette intensity: " + vignette.intensity.value);
+    }
+}
