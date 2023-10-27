577542a621880361469f91106f5916753402bc4d
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-10-27 22:39:36 +1300

    Add a helper function

---
+++ Assets/Scripts/UI/VisibilityToggler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityToggler : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObjectToToggle;
+    public void ToggleVisibility()
+    {
+        bool active = gameObjectToToggle.activeSelf;
+        gameObjectToToggle.SetActive(!active);
+    }
+}
