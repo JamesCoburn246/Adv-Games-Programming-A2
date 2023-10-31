5b049679b835020aa4bcd4bc601be4dc219a614e
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-11-01 10:32:32 +1300

    Remove old imports

--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 [RequireComponent(typeof(EnemyStats))]
 public class EnemyManager : MonoBehaviour
